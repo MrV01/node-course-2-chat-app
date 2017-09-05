f6314e2c79f76cb894cb6a23a6b40dd02ec4a8e4
MrV01 <VladV454@gmail.com>
2017-09-05 14:34:16 -0400

    Added form for messages and show incoming messages to browser

--- public/js/index.js
+++ public/js/index.js
@@ -18,12 +18,40 @@ socket.on('disconnect', function() {
 // Custom event listener newMessage
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
+  // Add message to the DOM  <ol id="messages"></ol>
+  var li = jQuery('<li></li>');
+  li.text(`${message.from} : ${message.text}`);
+  jQuery('#messages').append(li);
 });
 
 // Custom Event Emitter with Acknolegement to the client via callback function.
-socket.emit('createMessage', {
-   from: 'Frank',
-   text: 'Hi I am Frank'
-}, function (data) { // got some data from server back.
-  console.log('Got it :', data);
-});
+function sendChatMessage( fromName, messageText) {
+        socket.emit('createMessage', {
+             from: fromName,
+             text: messageText
+        }, function (data) { // get some data from server back.
+            console.log('Got it :', data);
+            return data;
+        });
+}
+//
+sendChatMessage("Freddy", "Good Day, I am Freddy. How are you?");
+
+///  JQuery Event handlers
+
+jQuery('#message-form').on('submit', function (ev) {
+
+  ev.preventDefault();   // JQuery to disable default form behavior
+  // get all the inputs into an array.
+   var $inputs = $('#message-form :input');
+   var values = {};
+   $inputs.each(function() {
+       values[this.name] = $(this).val();
+    });
+    // Send message if Any
+    var name = "Freddy " + location.host;
+    if(values.submit) {
+      sendChatMessage(name, values.message);
+    }
+
+} );
